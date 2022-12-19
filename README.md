9281ba0d202828b789b9bd6dcc47498a419e4a66
kumalabsconsulting <kumalabsconsulting@gmail.com>
2022-12-19 10:11:17 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # install-docker
-A simple script to install Docker on Linux
+A simple script to install Docker on Debian Linux
+
+## HOWTO
+
+```bash
+./install_docker myuser
+
+
+```
+
+This will install docker and add myuser in docker's group to launch docker commands
---
+++ install_docker.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# Simple script from Docker documentation
+# Add some extrat stuff
+# Author: J.CUADRADO <kumalabsconsulting> <redbeard28>
+
+## Check if Debian OS
+if test -r /etc/os-release
+  then
+	ID=`awk -F= '$1=="ID" { print $2 ;}' /etc/os-release`
+	VERSION=`awk -F= '$1=="VERSION" { print $2 ;}' /etc/os-release`
+else
+  echo "Don't know what os-type it is !"
+  exit 1
+fi
+
+echo "ID is $ID"
+if [[ "$ID" == 'debian' ]];then
+  echo $ID
+else
+  echo "This script is usefull for debian os"
+  exit 1
+fi
+
+
+#### Function
+docker_install_by_root () {
+  MYUSER=$1
+  apt-get remove docker docker-engine docker.io containerd runc
+
+  echo "Installing prerequisits..."
+  apt-get update
+  apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    virt-what
+  echo "Checking what type of host..."
+  HOST_TYPE=`virt-what`
+  if [[ "$HOST_TYPE" == 'lxc' ]];then
+	  echo "You are installing Docker on a $HOST_TYPE type"
+	  mkdir -p /etc/apt/keyrings
+	  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    usermod -a -G docker $MYUSER
+  fi
+}
+
+docker_install_by_others () {
+  MYUSER=$1
+  sudo apt-get remove docker docker-engine docker.io containerd runc
+
+  echo "Installing prerequisits..."
+  sudo apt-get update
+  sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    virt-what
+  echo "Checking what type of host..."
+  HOST_TYPE=`virt-what`
+  if [[ "$HOST_TYPE" == 'lxc' ]];then
+	  echo "You are installing Docker on a $HOST_TYPE type"
+	  sudo mkdir -p /etc/apt/keyrings
+	  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo usermod -a -G docker $MYUSER
+  fi
+}
+
+
+
+
+## MAIN
+if [[ "$EUID" -ne 0 ]];then
+
+  if [[ `which sudo` -ne 0 ]];then
+	  echo "There is no sudo command, please install..."
+    exit 1
+  fi
+else
+  docker_install_by_root "$1"
+fi
+
+
+
